26daad8240e416c9e6143c80459611e36056ed0f
Slavjan <khaidayev@gmail.com>
2016-02-29 01:15:24 +0600

    template func factorial

--- main.cpp
+++ main.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+template<typename T>
+T factorial(T n = 0){
+    T fact = 1;
+
+    if( n == 0 )
+        return 1;
+
+    if (n > 1) {
+        fact *= n * factorial(n-1);//recursive property
+    }
+
+    return fact;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+
+
+
+
+    cout << "Hello World!" << factorial(5) << endl;
     return 0;
 }
 
